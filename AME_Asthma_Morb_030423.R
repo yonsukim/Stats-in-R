721ad74b9a56285b9a8b660c42c59708e7a69793
Yonsu <yonsuk@uci.edu>
2023-04-07 13:42:35 -0700

    Updated

--- AME_Asthma_Morb_030423.R
+++ AME_Asthma_Morb_030423.R
@@ -13,86 +13,103 @@ library(readxl)
 ## library(ggeffects)
 ## update.packages("ggeffects")
 
-
+## install.packages("margins")
 ## library(margins)
 ## data(mtcars)
 
 #####################################################################
 
 
-### Example ###
-
-set.seed(123)
-y <- rbinom(300, 1, c(.3, .7))
-x <- rnorm(300, 2)
-y_1 <- y == 1
-x[y_1] <- x[y_1] + rnorm(sum(y_1), 3)
-
-d <- data.frame(x, y)
-model2 <- glm(y ~ x, family = binomial(), data = d)
-
-summary(model2)
-coef(model2)["x"]
-
-plot(ggpredict(model2, "x [all]"), ci = FALSE, add.data = TRUE)
-
-View(y_1)
-
 
 
 ##################### Social Mobility Data ##########################################
 
-mob <- read_excel("C:\\Users\\..........\\Mobility_data\\mob_02.xlsx") ### Needs to edit this path based on data location
+example <- read_excel("C:\\Users\\..........\\....xlsx") ### Needs to edit this path based on data location
+
+obs <- read_excel("C:\\Users\\Dongwoo\\Dropbox\\Research\\Asthma_Cormobdities\\Out\\out.xlsx", sheet="COMM_IP")
 
 
-# View(mob)
+# View(obs)
 # summary(mob)
 
+
+
 ## Convering DV to a numeric variable
-mob$health_n <- as.numeric(mob$health_b)
-mob$hap_n <- as.numeric(mob$hap)
-mob$mob2 <- as.numeric(mob$moc)
-
-## Ignore these statement
-# mob$mob2[mob$moc=="up"]<-1
-# mob$mob2[mob$moc=="st"]<-2
-# mob$mob2[mob$moc=="do"]<-3
+obs$bsev_n <- as.numeric(obs$bsev)
+obs$int_n <- as.numeric(obs$int)
+obs$pa_n <- as.numeric(obs$pa)
 
 
-## Assign reference group
-mob$moc <- factor(mob$moc, levels=c("st", "do", "up"))
-mob$mari <- factor(mob$mari, levels=c("M", "N", "D"))
-# mob$moc <- factor(mob$moc, levels=c("st", "do", "u"), ordered = TRUE)
-# mob$moc <- relevel(mob$moc, ref='st')
+
+obss = subset(obs, bsev_n !="9" & Race_val %in% c("2","3","4","5"))
+obso = subset(obs, bsev_n !="9" & Race_val %in% c("2","3","4","5") & AgeInYears > 18)
+obsy = subset(obs, bsev_n !="9" & Race_val %in% c("2","3","4","5") & AgeInYears < 19)
+# View(obss)
+
 
 
 ## Average Marginal Effect (AME) test with the unadjusted model
-model_01 <- glm(health_n ~ mob2, family = binomial(), data = mob)
-model_00 <- glm(health_n ~ moc, family = binomial(), data = mob)
 
-summary(model_00)  ## Gives the same results as SAS
-# confint(model_00)
-exp(cbind(OR=coef(model_00),confint(model_00)))  ## Gives the same results as SAS
+ip01 <- glm(bsev_n ~ obs, family = binomial(), data = obss)
+ip02 <- glm(int_n ~ obs, family = binomial(), data = obss)
+ip03 <- glm(pa_n ~ obs, family = binomial(), data = obss)
 
-plot(ggpredict(model_00, "moc [all]"), ci = FALSE, add.data = TRUE)
+ip11 <- glm(bsev_n ~ obs, family = binomial(), data = obso)
+ip12 <- glm(int_n ~ obs, family = binomial(), data = obso)
+ip13 <- glm(pa_n ~ obs, family = binomial(), data = obso)
 
-coef(model_00)["moc"]
+ip21 <- glm(bsev_n ~ obs, family = binomial(), data = obsy)
+ip22 <- glm(int_n ~ obs, family = binomial(), data = obsy)
+ip23 <- glm(pa_n ~ obs, family = binomial(), data = obsy)
 
-margins(model_00)
 
+summary(ip1)
+exp(cbind(OR=coef(ip1),confint(ip1)))  ## Gives the same results as SAS
+plot(ggpredict(ip01, "moc [all]"), ci = FALSE, add.data = TRUE)
 
- 
+coef(ip01)["1"]
+
+margins(ip01) ## .1477
+margins(ip02) ## .1649
+margins(ip03) ## .1284
+
+margins(ip11) ## .04029
+margins(ip12) ## .05528
+margins(ip13) ## .06106
+
+margins(ip21) ## .1596
+margins(ip22) ## .1986
+margins(ip23) ## .1929
+
 
 ###### Adjusted models 
 
-# SELF-RATED HEALTH
-model_05 <- glm(health_n ~ moc+sq1+aggrp+edui+hinc+rlgi+mari, family = binomial(), data = mob)
-model_05 <- glm(health_n ~ moc+sq1+aggrp+edui+hinc+mari, family = binomial(), data = mob)
-model_05 <- glm(health_n ~ moc+sq1+aggrp+edui+mari, family = binomial(), data = mob)
-model_05 <- glm(health_n ~ moc+sq1+aggrp+mari, family = binomial(), data = mob)
-model_05 <- glm(health_n ~ moc+sq1+aggrp, family = binomial(), data = mob)
-model_05 <- glm(health_n ~ moc+sq1, family = binomial(), data = mob)
-model_05 <- glm(health_n ~ moc+sq1+edui+hinc+rlgi+mari, family = binomial(), data = mob)
+ip31 <- glm(bsev_n ~ obs+age+race+Gender_val+medi, family = binomial(), data = obss)
+ip32 <- glm(int_n ~ obs+age+race+Gender_val+medi, family = binomial(), data = obss)
+ip33 <- glm(pa_n ~ obs+age+race+Gender_val+medi, family = binomial(), data = obss)
+
+ip41 <- glm(bsev_n ~ obs+age+race+Gender_val+medi, family = binomial(), data = obso)
+ip42 <- glm(int_n ~ obs+age+race+Gender_val+medi, family = binomial(), data = obso)
+ip43 <- glm(pa_n ~ obs+age+race+Gender_val+medi, family = binomial(), data = obso)
+
+ip51 <- glm(bsev_n ~ obs+race+Gender_val+medi, family = binomial(), data = obsy)
+ip52 <- glm(int_n ~ obs+race+Gender_val+medi, family = binomial(), data = obsy)
+ip53 <- glm(pa_n ~ obs+race+Gender_val+medi, family = binomial(), data = obsy)
+
+
+margins(ip31) #.04892
+margins(ip32) #.08302
+margins(ip33) #.09353
+
+margins(ip41) # .04915
+margins(ip42) # .06662
+margins(ip43) # .07563
+
+margins(ip51) # .1551
+margins(ip52) # .1837
+margins(ip53) # .1819
+
+
 
 summary(model_05)  ## Gives the same results as SAS
 exp(cbind(OR=coef(model_05),confint(model_05)))  ## Gives the same results as SAS
@@ -106,58 +123,8 @@ summary(margins(model_05, variables = c("moc", "sq1", "aggrp")))
 summary(margins(model_05, variables = c("edui","hinc","rlgi","mari")))
 
 
-# Happiness
-model_15 <- glm(hap_n ~ moc+sq1+aggrp+edui+hinc+rlgi+mari, family = binomial(), data = mob)
-model_15 <- glm(hap_n ~ moc+sq1+aggrp+edui+hinc+mari, family = binomial(), data = mob)
-model_15 <- glm(hap_n ~ moc+sq1+aggrp+edui+mari, family = binomial(), data = mob)
-model_15 <- glm(hap_n ~ moc+sq1+aggrp+mari, family = binomial(), data = mob)
-model_15 <- glm(hap_n ~ moc+sq1+aggrp, family = binomial(), data = mob)
-model_15 <- glm(hap_n ~ moc+sq1, family = binomial(), data = mob)
-model_15 <- glm(hap_n ~ moc+sq1+edui+hinc+rlgi+mari, family = binomial(), data = mob)
-
-
-summary(model_15)  ## Gives the same results as SAS
-exp(cbind(OR=coef(model_15),confint(model_15)))  ## Gives the same results as SAS
-
-plot(ggpredict(model_15, "moc [all]"), ci = FALSE, add.data = TRUE)
-
-margins(model_15)
-
-## Summary function provides AME, SE, Z, P, CI - Divided into two groups
-summary(margins(model_15, variables = c("moc", "sq1", "aggrp")))
-summary(margins(model_15, variables = c("edui","hinc","rlgi","mari")))
-
-
-#### END OF THE TEST - 03/09/2023 ####
 
 
 
-################ APPENDIX: Example for logistic regression with categorical variables
-## https://strengejacke.github.io/ggeffects/articles/introduction_marginal_effects.html
-
-smoking <- data.frame(
-  sex = factor(c("male", "female", "female", "male", "female", "female",
-                 "male", "female", "female", "male", "male", "female",
-                 "female"), 
-               levels = c("male", "female")),
-  smoking = factor(c("no", "yes", "yes", "yes", "yes", "no", "no", "yes",
-                     "yes", "no", "no", "no", "yes"), 
-                   levels = c("no", "yes")),
-  age = c(10, 45, 50, 40, 45, 12, 14, 55, 60, 10, 14, 50, 40)
-)
-
-
-100 * prop.table(table(smoking$sex, smoking$smoking), margin = 1)
-
-
-model5 <- glm(smoking ~ sex, family = binomial(), data = smoking)
-
-model_parameters(model5, exponentiate = TRUE)
-
-ggpredict(model5, "sex")
-
-margins(model5)
-
-
-model6 <- glm(smoking ~ sex + age, family = binomial(), data = smoking)
+#### END OF THE TEST - 04/07/2023 ####
 
