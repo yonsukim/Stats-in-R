ef989611c7cd0fd00d02eccf51659a65e47151c9
yonsukim <62728287+yonsukim@users.noreply.github.com>
2023-04-12 15:03:02 -0700

    First

--- AME_Asthma_Morb_030423.R
+++ AME_Asthma_Morb_030423.R
@@ -1,21 +1,21 @@
 ### Average Marginal Effect Model - ASTHMATIC DIABETES  04/07/23 ###
-
+ 
 
 ##################### Packages to be installed #####################
 
 install.packages("readxl")
 library(readxl)
 
-## install.packages("broom")
-## library(broom)
+install.packages("broom")
+library(broom)
 
-## install.packages("ggeffects")
-## library(ggeffects)
-## update.packages("ggeffects")
+install.packages("ggeffects")
+library(ggeffects)
+update.packages("ggeffects")
 
-## install.packages("margins")
-## library(margins)
-## data(mtcars)
+install.packages("margins")
+library(margins)
+data(mtcars)
 
 #####################################################################
 
@@ -35,13 +35,36 @@ obs <- read_excel("C:\\Users\\.....\\Dropbox\\Research\\Asthma_Cormobdities\\Out
 obs$bsev_n <- as.numeric(obs$bsev)
 obs$int_n <- as.numeric(obs$int)
 obs$pa_n <- as.numeric(obs$pa)
+obs$sev_n <- as.numeric(obs$sev)
 
 
 ## Creating subgroups by ages - subset function
 obss = subset(obs, bsev_n !="9" & Race_val %in% c("2","3","4","5"))
 obso = subset(obs, bsev_n !="9" & Race_val %in% c("2","3","4","5") & AgeInYears > 18)
 obsy = subset(obs, bsev_n !="9" & Race_val %in% c("2","3","4","5") & AgeInYears < 19)
+
+### Tables including the levels of BMI
+obsy0 = subset(obs, Race_val %in% c("2","3","4","5") & AgeInYears > 18 & sev_n %in% c("0","9"))
+obsy1 = subset(obs, Race_val %in% c("2","3","4","5") & AgeInYears > 18 & sev_n %in% c("1","9"))
+obsy2 = subset(obs, Race_val %in% c("2","3","4","5") & AgeInYears > 18 & sev_n %in% c("2","9"))
+obsy3 = subset(obs, Race_val %in% c("2","3","4","5") & AgeInYears > 18 & sev_n %in% c("3","9"))
 # View(obss)
+
+
+### Values converted
+obsy0$sev_n <- ifelse(obsy0$sev_n == 0, 1, obsy0$sev_n)
+obsy0$sev_n <- ifelse(obsy0$sev_n == 9, 0, obsy0$sev_n)
+
+obsy1$sev_n <- ifelse(obsy1$sev_n == 1, 1, obsy1$sev_n)
+obsy1$sev_n <- ifelse(obsy1$sev_n == 9, 0, obsy1$sev_n)
+
+obsy2$sev_n <- ifelse(obsy2$sev_n == 2, 1, obsy2$sev_n)
+obsy2$sev_n <- ifelse(obsy2$sev_n == 9, 0, obsy2$sev_n)
+
+obsy3$sev_n <- ifelse(obsy3$sev_n == 3, 1, obsy3$sev_n)
+obsy3$sev_n <- ifelse(obsy3$sev_n == 9, 0, obsy3$sev_n)
+
+# View(obsy1)
 
 
 ## Average Marginal Effect (AME) test with the unadjusted models
@@ -102,6 +125,16 @@ ip51 <- glm(bsev_n ~ obs+race+Gender_val+medi, family = binomial(), data = obsy)
 ip52 <- glm(int_n  ~ obs+race+Gender_val+medi, family = binomial(), data = obsy)
 ip53 <- glm(pa_n   ~ obs+race+Gender_val+medi, family = binomial(), data = obsy)
 
+## Logistic regression with BMI
+ip60 <- glm(sev_n ~ bmi+age+race+Gender_val+medi, family = binomial(), data = obsy0)
+ip61 <- glm(sev_n ~ bmi+age+race+Gender_val+medi, family = binomial(), data = obsy1)
+ip62 <- glm(sev_n ~ bmi+age+race+Gender_val+medi, family = binomial(), data = obsy2)
+ip63 <- glm(sev_n ~ bmi+age+race+Gender_val+medi, family = binomial(), data = obsy3)
+
+summary(ip60)
+summary(ip61)
+summary(ip62)
+summary(ip63)
 
 margins(ip31) #.1255  .04892
 margins(ip32) #.146   .08302
@@ -114,6 +147,11 @@ margins(ip43) #.07563
 margins(ip51) #.1551
 margins(ip52) #.1837
 margins(ip53) #.1819
+
+margins(ip60)
+margins(ip61)
+margins(ip62)
+margins(ip63)
 
 
 # Summary tables, OR, and CI
@@ -132,6 +170,12 @@ summary(margins(ip43, variables = c("obs")))
 summary(margins(ip51, variables = c("obs")))
 summary(margins(ip52, variables = c("obs")))
 summary(margins(ip53, variables = c("obs")))
+
+summary(margins(ip60, variables = c("bmi")))
+summary(margins(ip61, variables = c("bmi")))
+summary(margins(ip62, variables = c("bmi")))
+summary(margins(ip63, variables = c("bmi")))
+
 
 #### END OF THE TEST - 04/07/2023 ####
 
